6858ede65c9388a028c54e3ea5540113bca8ff2e
Yuvraj-Mottan <yuvrajmottan27@gmail.com>
2026-03-17 18:33:25 +0530

    Initial frame

---
+++ main.cpp
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+#include <bits/stdc++.h>
+using namespace std;
+struct instruction
+{
+    string name;
+    int opCode;
+    string type;
+};
+
+int main(){  
+
+    return 0;
+=======
+#include <bits/stdc++.h>
+using namespace std;
+struct instruction
+{
+    string name;
+    int opCode;
+    string type;
+};
+
+int main(){  
+
+    return 0;
+>>>>>>> 7625c09 (initial commit)
+}
